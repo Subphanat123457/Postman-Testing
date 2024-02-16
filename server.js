b5e5e7f3154778f087b79309ef304803e7c08d9c
MFL <irpg17102545@gmail.com>
2024-02-16 11:20:40 +0700

    fix bug app

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const app = express()
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
-
-const app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello World')
