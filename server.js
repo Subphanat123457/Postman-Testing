7861c3b59233367521f8f245af497a7131d5f8f6
MFL <irpg17102545@gmail.com>
2024-02-16 11:39:48 +0700

    fix status req delete

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ app.delete('/books/:id', (req, res) => {
     const deletedIndex = books.findIndex(book => book.id === req.params.id)
         //   #  books.splice(deletedIndex, 1)
     delete books[deletedIndex];
-    // res.status(200).json(req.body)
+    res.status(200).json(req.body)
 })
