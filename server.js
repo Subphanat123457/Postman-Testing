8c823b69be637ee3d2b6e9b1afd21301f21a4c90
MFL <irpg17102545@gmail.com>
2024-02-16 10:38:29 +0700

    book-id

--- server.js
+++ server.js
@@ -10,16 +10,18 @@ app.listen(3000, () => {
     console.log('http://localhost:3000')
 })
 
+// Select * from books where id = 1?
+app.get('/books/:id', (req, res) => {
+    res.json(books.find(book => book.id === req.params.id))
+})
+
 // Select * from books
 const books = require('./db')
 app.get('/books', (req, res) => {
     res.json(books)
 })
 
-// Select * from books where id = 1?
-app.get('/books/:id', (req, res) => {
-    res.json(books.find(book => book.id === req.params.id))
-})
+
 
 // Insert into books (id,name) values ("3","Crazy horse")
 app.post('/books', (req, res) => {
