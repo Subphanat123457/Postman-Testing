ed147f0a06c8b58aeca2d82c76834e136cd93b91
MFL <irpg17102545@gmail.com>
2024-02-16 10:19:11 +0700

    db.json

---
+++ db.js
@@ -0,0 +1,13 @@
+[{
+        "id": "1",
+        "name": "Operating System"
+    },
+    {
+        "id": "2",
+        "name": "Software Engineering"
+    },
+    {
+        "id": "3",
+        "name": "Multimedia Programming"
+    }
+]
